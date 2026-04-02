ea688592c22328468ec19391d2ade43ebfd9a5f8
TsurayaHasnaWindya <rayahasna3@gmail.com>
2026-04-02 17:27:22 +0700

    update motor

--- app/Filament/Resources/MotorResource.php
+++ app/Filament/Resources/MotorResource.php
@@ -99,8 +99,7 @@ class MotorResource extends Resource
 
             TextColumn::make('sewa_perhari')
                 ->label('Harga')
-                ->formatStateUsing(fn (string|int|null $state): string => rupiah($state))
-                ->extraAttributes(['class' => 'text-right']) // Tambahkan kelas CSS untuk rata kanan
+                ->formatStateUsing(fn ($state) => 'Rp ' . number_format($state,0,',','.'))
                 ->sortable()
 
         ])
