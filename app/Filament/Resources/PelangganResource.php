a03a1e511163c37f9bd1fab28558e9b2cf74f66a
zahrarani122-beep <zahrarani122@gmail.com>
2026-04-09 09:25:40 +0700

    Percobaan Keempat

--- app/Filament/Resources/PelangganResource.php
+++ app/Filament/Resources/PelangganResource.php
@@ -89,7 +89,7 @@ class PelangganResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make(), 
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
